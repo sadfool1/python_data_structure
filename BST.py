6af69261005478cd4f5fbbd2f659ee64854dd76c
James <33245652+sadfool1@users.noreply.github.com>
2019-04-06 19:05:43 +0800

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,81 @@
+'''
+Lab 3 Task 4 
+'''
+
+
+'''
+=========================================
+We implement the Binary Search Tree clsss
+=========================================
+'''
+
+class BSTNode: 
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None 
+        
+class BinarySearchTree:  
+    def __init__(self): 
+        self.root = None  
+        self.size = 0
+
+    def insert(self,data): 
+        # create a new node 
+        newNode = BSTNode(data)
+        
+        # inser the new node into the binary search tree
+        
+        # if the tree is empty, the new node is set as root.
+        if self.root == None:   
+            self.root=newNode 
+            self.size+=1
+            return
+        # if the tree is not empty, we try to find the 
+        # value of of the input data in the BST 
+        # if the input data is already in the BST, do nothing
+        # if we can not find the input data in the BST, 
+        # insert the new node at the empty node we reached.   
+        curr=self.root
+        while curr:
+            # if data exists in the tree already, do nothing, return 
+            if newNode.data == curr.data:
+                return
+            # if data is larger than the current node data,
+            # go to right child, if right child is empty, insert node. 
+            elif newNode.data > curr.data: 
+                if curr.rightChild != None: 
+                    curr = curr.rightChild
+                else:
+                    curr.rightChild = newNode
+            # if data is smaller than the current node data,
+            # go to left child, if left child is empty, insert node. 
+            else:  
+                if curr.leftChild != None: 
+                    curr = curr.leftChild
+                else:
+                    curr.leftChild = newNode  
+        self.size+=1
+        return 
+    
+    def inorderTraversal(self, root): 
+        result = [] 
+        if root: 
+            result = self.inorderTraversal(root.leftChild)
+            result.append(root.data)
+            result = result + self.inorderTraversal(root.rightChild)
+        return result
+
+'''
+=============================================
+'''
+
+list1 = [7,4,5,3,1,8,6,10] 
+
+myBST = BinarySearchTree()
+for x in list1:
+    myBST.insert(x) 
+
+r = myBST.inorderTraversal(myBST.root)
+print(r)   
+
---
+++ Queue_node.py
@@ -0,0 +1,59 @@
+'''
+Lab 3 Task 2
+'''
+
+class QueueNode: 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+We implement the queue similar to the linked list 
+The front is the first node of the linked list 
+The rear is the last node of the linked list 
+'''        
+class Queue: 
+    def __init__(self):
+        self.front = None
+        self.rear = None 
+        self.size = 0 
+
+    def enqueue(self,data):
+        newNode = QueueNode(data)
+        # enqueue when the queue is empty
+        if self.front == None:  
+            self.front=newNode 
+            self.rear=newNode
+        # enqueue when the queue is not empty 
+        else:
+            self.rear.next=newNode 
+            self.rear = newNode 
+        self.size += 1
+ 
+    def dequeue(self):
+        # if que is empty, cannot dequeue, return None 
+        if self.front == None:  
+            return None 
+        else: 
+            data = self.front.data 
+            self.front = self.front.next
+            if self.front == None:
+                self.rear = None 
+            self.size -= 1 
+            return data 
+ 
+    def printNode(self):
+        curr = self.front
+        while curr:
+            print(curr.data)
+            curr = curr.next 
+
+myqueue = Queue() 
+myqueue.enqueue(3)
+myqueue.enqueue(5)
+myqueue.enqueue(9)
+myqueue.dequeue()
+myqueue.enqueue(10)
+myqueue.dequeue()
+myqueue.printNode()
+
